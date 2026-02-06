a8977011346d36360d8dd8fd25032d15acb98a9d
shinyatlas <mannyoso07@gmail.com>
2026-02-06 13:05:57 -0500

    first commit to repo

---
+++ main.m
@@ -0,0 +1,7 @@
+%select the data file using the standard windows file viewer
+[filename, pathname] = uigetfile('*.csv', 'Select dataset');
+full_path = fullfile(pathname, filename);
+
+%load data into matlab 
+readtable(full_path)
+
