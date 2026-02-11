51f7b387f1a6d3b799d9a26acc3bf34a2c3c674d
shinyatlas <mannyoso07@gmail.com>
2026-02-11 13:42:10 -0500

    Added arguement validation to the model

--- cytokinemodel.m
+++ cytokinemodel.m
@@ -1,16 +1,19 @@
-function [outputArg1,outputArg2] = cytokinemodel(inputArg1,inputArg2)
+function [outputArg1,outputArg2, outputArg3] = cytokinemodel(t,y,p)
 %Comprehensive model for cytokine modeling
 %   Detailed explanation goes here
 arguments (Input)
-    inputArg1
-    inputArg2
+    t (1,1) double %time (must be a scalar)
+    y (3,1) double %current cell count at a given t-value
+    p CytokineParameter %an instance of the CytokineParameter class
 end
 
 arguments (Output)
     outputArg1
     outputArg2
+    outputArg3
 end
 
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+outputArg1 = t;
+outputArg2 = y;
+outputArg3 = p;
 end
